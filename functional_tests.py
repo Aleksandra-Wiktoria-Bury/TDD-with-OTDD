df8af1ec222278389bd7b4a7249610ea04305696
Aleksandra Bury <abury@thinkport.digital>
2022-05-09 11:24:29 +0200

    folder name change fix

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 
+
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
+
+# an assertion is a predicate connected to a point in the program, that always should evaluate to true at that point in code execution
+# checks for the default Django "it worked" page
 assert 'Django' in browser.title
