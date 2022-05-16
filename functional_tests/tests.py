249aecd914bbc4bcf89f4b857f892c9c5fc25ff0
Aleksandra Bury <abury@thinkport.digital>
2022-05-16 10:33:06 +0200

    Multiple users FT written

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import WebDriverException
 
-#import unittest
 MAX_WAIT = 10
 class NewVisitorTest (LiveServerTestCase):
 
@@ -14,6 +13,7 @@ class NewVisitorTest (LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # helper method:
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -27,7 +27,7 @@ class NewVisitorTest (LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # user checks the homepage
         self.browser.get(self.live_server_url)
         
@@ -59,7 +59,44 @@ class NewVisitorTest (LiveServerTestCase):
         self.wait_for_row_in_list_table('1. Buy new carpet')
         self.wait_for_row_in_list_table('2. Mount the lamp')
 
+    def test_multiple_users_can_start_lists_with_diff_URL(self):
+
+        # userA starts a new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy new carpet')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy new carpet')
+
         # will the results be stored? There should be a custom generated URL
+        userA_list_URL = self.browser.current_url
+        self.assertRegex(userA_list_URL, '/lists/.+')
+
+        ##! new user visits - use new browser to make sure no info i.e. through cookies is present
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # userB visits page, there's no trace of userA list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy new carpet', page_text)
+        self.assertNotIn('Mount the lamp', page_text)
+
+        # userB starts a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1.  Buy milk')
+
+        # userB gets own URL
+        userB_list_URL = self.browser.current_url
+        self.assertRegex(userB_list_URL, '/lists/.+')
+        self.assertNotEqual(userA_list_URL, userB_list_URL)
+
+        # again no trace of userA list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy new carpet', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # user visits that URL - her to-do list is still there.
         self.fail('Finish the test!')
