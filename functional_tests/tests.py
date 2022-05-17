e8619d7eee887399eae386f40859698cff15f914
Aleksandra Bury <abury@thinkport.digital>
2022-05-17 11:56:15 +0200

    start FT for layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -52,8 +52,11 @@ class NewVisitorTest (LiveServerTestCase):
 
         # input form for adding a new task is still present, user enters another task
         inputbox = self.browser.find_element_by_id('id_new_item')
+         # input box is centered
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta = 10)
         inputbox.send_keys('Mount the lamp')
         inputbox.send_keys(Keys.ENTER)
+
 
         # upon page update, 2 tasks are displayed
         self.wait_for_row_in_list_table('1. Buy new carpet')
@@ -99,8 +102,17 @@ class NewVisitorTest (LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # user visits that URL - her to-do list is still there.
-        self.fail('Finish the test!')
+
         # all achieved!
+    def test_layout_and_styling(self):
+
+        # user goes to the webpage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta = 10)
 
 #if __name__ == '__main__':  
 #    unittest.main()
